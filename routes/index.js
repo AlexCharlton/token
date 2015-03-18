d05b2313f09aaa34e52f0727c2be3a3b1aca4217
Alex Charlton <alex.n.charlton@gmail.com>
2015-03-18 17:23:38 -0400

    Remove some unused paths

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,6 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-// router.get('/about', function(req, res, next) {
-//     res.render('about');
-// });
-
-// router.get('/tags', function(req, res, next) {
-//     res.render('index', { title: 'Token' });
-// });
-
-// router.get('/tags/:tag', function(req, res, next) {
-//     // Chunk results
-//     res.render('index', { title: 'Token' });
-// });
-
-// router.get('/org', function(req, res, next) {
-//     // Results are chuncked
-//     res.render('index', { title: 'Token' });
-// });
-
 // router.get('/org/:org', function(req, res, next) {
 //     res.render('logo');
 // });
